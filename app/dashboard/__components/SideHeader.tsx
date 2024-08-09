0258bdf90cc84039cad4ef99c8ab4aa265831f83
Rufai-Ahmed <abbeyrufai234@gmail.com>
2024-08-09 22:59:35 +0100

    mobile sidebar added

--- app/dashboard/__components/SideHeader.tsx
+++ app/dashboard/__components/SideHeader.tsx
@@ -1,12 +1,64 @@
+"use client";
+import Sidebar from "@/app/__components/static/Sidebar";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { iSidebar } from "@/interfaces";
+import { sidebarData } from "@/lib/data/data";
 import { Menu, Search } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { FaAngleDown } from "react-icons/fa6";
 
 const SideHeader = () => {
+  const path = usePathname();
+  const isActive: (name: string) => boolean = (name: string) => name === path;
+
   return (
-    <header className="h-[90px] px-8 w-full border-b-[2px] border-[#232323] flex items-center justify-between">
-      <Menu size={20} className="lg:hidden" />
+    <header className="h-[60px] md:h-[90px] px-8 w-full border-b-[2px] border-[rgb(35,35,35)] flex items-center justify-between">
+      <div className="lg:hidden flex items-center">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Menu size={25} className="lg:hidden" />
+          </SheetTrigger>
+          <SheetContent
+            className="text-[#c0bcbc] overflow-y-auto"
+            side={"left"}
+          >
+            <SheetHeader>
+              <SheetTitle>Smartwave</SheetTitle>
+              <SheetDescription></SheetDescription>
+            </SheetHeader>
+
+            <div className="space-y-3 w-full">
+              {sidebarData.map((el: iSidebar, i: number) => (
+                <Link
+                  href={el.path!}
+                  key={i}
+                  className={`w-full  relative flex items-center font-light pl-8 text-[15px] cursor-pointer duration-300 gap-4 h-[50px] ${
+                    isActive(el.path!)
+                      ? "bg-[#6E18BB] text-white"
+                      : "text-[#B5B5B5] hover:text-white hover:bg-[#6E18BB]"
+                  }`}
+                >
+                  {isActive(el.path!) && (
+                    <div className="absolute w-2 rounded-r-lg bg-[#F29F05] h-full left-0" />
+                  )}
+                  {el.icon} <p className="capitalize">{el.name}</p>
+                  {el.drop && <FaAngleDown size={15} />}
+                </Link>
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
 
       <div className="sm:flex items-center gap-3 px-3 h-[45px] hidden  md:w-[400px] bg-[#191919] rounded-lg">
         <Search size={18} />
