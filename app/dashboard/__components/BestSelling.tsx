0339b909eabacdb87d628157f38911cbd2468a99
Rufai-Ahmed <abbeyrufai234@gmail.com>
2024-08-10 19:43:31 +0100

    added other grid page and applied responsiveness

---
+++ app/dashboard/__components/BestSelling.tsx
@@ -0,0 +1,61 @@
+import Button from "@/app/__components/Button";
+import Image from "next/image";
+import React, { FC } from "react";
+
+const BestSelling: FC<{ heading?: boolean }> = ({ heading }) => {
+  return (
+    <div className="col-span-4 xl:col-span-1">
+      <h3 className="text-[18px] font-bold mb-5">
+        {heading && "Best Selling"}
+      </h3>
+      <section
+        className={`w-full border rounded-[10px] p-3 flex flex-col justify-between border-gray-500 h-[270px] `}
+      >
+        <div className="flex text-[14px] items-center gap-3">
+          <Image
+            src={"/assets/images/diff_winter.jpg"}
+            width={100}
+            height={100}
+            alt="Book called Diff winter"
+            className="w-[80px] rounded-md h-auto"
+          />
+
+          <div className="h-full flex flex-col justify-between">
+            <div>
+              <p>
+                <b>Unknown Bold Long</b>
+              </p>
+              <p>Unknown Short</p>
+            </div>
+
+            <div>Lorem ipsum dolor sit.</div>
+          </div>
+        </div>
+
+        <div className="space-y-1 text-[14px]">
+          <p>
+            <b>Lorem, ipsum.</b>
+          </p>
+
+          <p className="font-light">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius
+            expedita laudantium officiis?
+          </p>
+
+          <p className="text-orange-500">Lorem ipsum dolor sit.</p>
+        </div>
+
+        <div className="grid gap-2 grid-cols-2">
+          <Button className=" text-[14px] md:text-[15px] h-auto md:h-auto w-full 2xl:w-full md:w-full">
+            Lorem, ipsum.
+          </Button>
+          <Button className="bg-transparent text-[14px] md:text-[15px] h-auto md:h-auto w-full 2xl:w-full md:w-full">
+            Lorem, ipsum.
+          </Button>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default BestSelling;
--- app/dashboard/__components/Homescreen.tsx
+++ app/dashboard/__components/Homescreen.tsx
@@ -5,6 +5,8 @@ import { BiMoneyWithdraw } from "react-icons/bi";
 import { GiGrowth } from "react-icons/gi";
 import { RiCurrencyFill } from "react-icons/ri";
 import { TbGrowth } from "react-icons/tb";
+import QuickAction from "./QuickAction";
+import BestSelling from "./BestSelling";
 
 const Homescreen = () => {
   return (
@@ -25,7 +27,7 @@ const Homescreen = () => {
       </div>
 
       <div className="w-full grid mt-5 text-white grid-cols-12 gap-5">
-        <div className="h-[270px] bg-[#6E18BB] rounded-[20px] xl:col-span-4 col-span-12 p-5 flex flex-col justify-center overflow-hidden relative">
+        <div className="min-h-[270px] bg-[#6E18BB] rounded-[20px] xl:col-span-4 col-span-12 p-5 flex flex-col justify-center overflow-hidden relative">
           <div className="absolute rounded-full size-96 scale-110 -left-24 bg-[#a567da]" />
 
           <div className="z-10 w-full h-full flex flex-col justify-between">
@@ -48,11 +50,11 @@ const Homescreen = () => {
             </Button>
           </div>
         </div>
-        <div className="h-[270px] grid grid-cols-1 md:grid-cols-3 md:grid-rows-2 col-span-12 xl:col-span-8 gap-5">
+        <div className="min-h-[270px] grid grid-cols-1 md:grid-cols-3 md:grid-rows-2 col-span-12 xl:col-span-8 gap-5">
           {Array.from({ length: 6 }).map((el: unknown, i: number) => (
             <div
               key={i}
-              className="border h-[100px] flex flex-col justify-between md:h-auto p-3 rounded-[10px]"
+              className="border h-[100px] flex flex-col justify-between md:h-auto p-3 border-gray-500 rounded-[10px]"
             >
               <div className="w-full flex justify-between">
                 <p className="font-light">Unknown Text</p>
@@ -78,6 +80,13 @@ const Homescreen = () => {
               </p>
             </div>
           ))}
+        </div>
+      </div>
+      <div className="grid mt-5 items-end grid-cols-1 gap-5 lg:grid-cols-3">
+        <QuickAction />
+        <div className="col-span-3 xl:col-span-2 gap-4 items-end grid grid-cols-4 xl:grid-cols-2">
+          <BestSelling heading={true} />
+          <BestSelling />
         </div>
       </div>
     </section>
